951a48643627fac7f0e9ae8bb1d8bf92c42f59e3
fl & teresa <student@epicodus.com>
2015-10-27 15:52:10 -0700

    Add check item in click listener

--- ParseStarterProject/src/main/java/com/parse/starter/UserList.java
+++ ParseStarterProject/src/main/java/com/parse/starter/UserList.java
@@ -2,9 +2,14 @@ package com.parse.starter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -23,9 +28,24 @@ public class UserList extends AppCompatActivity {
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_checked, users);
 
         ListView listView = (ListView) findViewById(R.id.listView);
-
+        listView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
         listView.setAdapter(arrayAdapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                CheckedTextView checkedTextView = (CheckedTextView) view;
+                    if (checkedTextView.isChecked()){
+                        Log.i("AppInfo", "Row is checked");
+
+                } else {
+
+                        Log.i("AppInfo", "Row is not checked");
+                    }
+
+            }
+        });
 
     }
 
